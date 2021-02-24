932a7071f3baa2856065c9d73a429633ecab0bb3
최은정 <ejchoi@infrawareglobal.com>
2021-02-24 15:53:00 +0900

    JKTJ-62 추가

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Nice to meet you~");
-        System.out.println("Bye^^")
+        System.out.println("Bye^^")~~~
     }
 }
